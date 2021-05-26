6028aa21e2e6be9cc67f562a47e9ea12b8e6e209
sayani2014 <sayanikoley8@gmail.com>
2021-05-26 13:25:19 +0530

    Display Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,33 @@
 #! /bin/bash -x
 
-flipCoin=$((RANDOM%2))
-if(($flipCoin==1))
-then
-	echo "HEAD"
-else
-	echo "TAIL"
-fi		
+declare -A singlet
+countHead=0
+countTail=0
+
+read -p "Enter the number of times you want to flip the coin: " number
+
+for((i=1;i<=number;i++))
+do
+	flipCoin1=$((RANDOM%2))
+
+	#Singlet combo
+	if(($flipCoin1==1))
+	then
+		countHead=$(($countHead+1))
+		singlet[$i]="H"
+	else
+		countTail=$(($countTail+1))
+		singlet[$i]="T"
+	fi
+done
+
+echo "Singlet dictionary: " "${singlet[@]}"
+
+percentageHead=$(($countHead * 100 / $number))
+echo -n "Percentage of HEAD in the singlet dictionary is: " 
+echo "scale=2;$percentageHead" | bc
+
+percentageTail=$(($countTail * 100 / $number))
+echo -n "Percentage of TAIL in the singlet dictionary is: " 
+echo "scale=2;$percentageTail" | bc
+		
